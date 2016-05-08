4a178b1d5cdd99aa5fc2d92dde0161fe896657a9
Linus Liang <linus.h.liang@gmail.com>
2016-05-08 16:04:23 -0400

    updated gitignore

--- main.py
+++ main.py
@@ -12,4 +12,14 @@ with open('secrets.json') as data_file:
     token_secret = data['token_secret']
 
     twitter = TwitterAPI(consumer_key, consumer_secret, token_key, token_secret)
-    followers = twitter.get_followers("bonobos")["users"]
+    response = twitter.get_followers("bonobos")
+
+    followers = response['users']
+    next_cursor = response['next_cursor']
+
+
+    with open('cursor.txt', 'w') as f:
+       f.write("{}".format(next_cursor))
+
+    with open('bonobos_followers.json', 'a') as f:
+        f.write("{}".format(followers))
