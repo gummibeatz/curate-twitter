25051ebd03c14c9f51f764fba2e158f9c11a0b07
Linus Liang <linus.h.liang@gmail.com>
2016-05-08 15:45:47 -0400

    updated gitignore and moved oauth secrets to secrets.json

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from twitter_client import TwitterAPI
+import json
 
 
+with open('secrets.json') as data_file:    
+    data = json.load(data_file)
 
-consumer_key = "8mmEGIIT9UOqcNq3l4k3ct32R"
-consumer_secret = "axEFlGEDzwix7tlwaOkBmSfWAOYEYcdK5eADXncUsHWeb4u00v"
+    consumer_key = data['consumer_key']
+    consumer_secret = data['consumer_secret']
 
-token_key = "701538848250191872-3ZFf4v4KJuWyACQ1cQTrFakKKKjASE3"
-token_secret = "NphMo0nU1b3tQwCH80SF88PYZUjIUFlt3uTOBeHH2JdIW"
+    token_key = data['token_key']
+    token_secret = data['token_secret']
 
-twitter = TwitterAPI(consumer_key, consumer_secret, token_key, token_secret)
-followers = twitter.get_followers("bonobos")["users"]
+    twitter = TwitterAPI(consumer_key, consumer_secret, token_key, token_secret)
+    followers = twitter.get_followers("bonobos")["users"]
