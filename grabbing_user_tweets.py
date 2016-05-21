715e60775d60e3a923d63ff3340e8ee944191e41
Linus Liang <linus.h.liang@gmail.com>
2016-05-21 18:03:47 -0400

    clean up

--- grabbing_user_tweets.py
+++ grabbing_user_tweets.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 from twitter_client import TwitterAPI
+from follower_parser import FollowerParser
 import json
 import os.path
 
@@ -16,4 +17,20 @@ with open('/Volumes/LINUS_USB/Twitter-Scraper/secrets.json') as data_file:
 
     twitter = TwitterAPI(consumer_key, consumer_secret, token_key, token_secret)
     
-    response = twitter.get_user_timeline("gummibeatz")
+    line_number = 0
+    if os.path.isfile('/Volumes/LINUS_USB/Twitter-Scraper/line_number'):
+        line_number = open('/Volumes/LINUS_USB/Twitter-Scraper/line_number').read()
+
+    list_index = 0
+    if os.path.isfile('/Volumes/LINUS_USB/Twitter-Scraper/list_index'):
+        list_index = open('/Volumes/LINUS_USB/Twitter-Scraper/list_index').read()
+
+    # go through each user
+    # save row at which you are at
+    # save index of list that you are at
+    # stop after 100 requests
+    filename = '/Volumes/LINUS_USB/Twitter-Scraper/followers.json'
+
+    parser = FollowerParser(line_number, filename)
+    follower = parser.get_follower(list_index)
+    print(follower.name())
