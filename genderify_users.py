30cce6d30163962755f5116ffea5ac00d00520d7
Linus Liang <linus.h.liang@gmail.com>
2016-08-06 13:12:27 -0400

    added script to genderify csvs

---
+++ genderify_users.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+
+
+# appends an additional column to csv signaling the user gender
+
+import sexmachine.detector as gender
+import sys
+import io
+from unicode_csv import *
+
+def get_first_name(full_name):
+    if full_name:
+        return full_name.split()[0].split('.')[0]
+    else:
+        return "N/A"
+
+twitter_handle = sys.argv[-1]
+
+csv_file_in = "{0}/{1}_results.csv".format(twitter_handle, twitter_handle)
+csv_file_out = "{0}/{1}_results_genderified.csv".format(twitter_handle, twitter_handle) 
+d = gender.Detector(case_sensitive=False)
+
+genderified_list = []
+with io.open(csv_file_in, 'rb') as csv_data:
+    reader = UnicodeReader(csv_data)
+
+    for row in reader:
+        row.append(d.get_gender(get_first_name(row[0])))
+        genderified_list.append(row)
+
+with io.open(csv_file_out, 'wb') as csv_data:
+    writer = UnicodeWriter(csv_data)
+    writer.writerows(genderified_list)
