ab262411b30e1573cb9552bfd2d5b499cb047a99
Linus Liang <linus.h.liang@gmail.com>
2016-09-19 21:31:49 -0400

    finished up parse tweets for regular style

--- parse_tweets.py
+++ parse_tweets.py
@@ -9,11 +9,34 @@ query = sys.argv[-1]
 tweets_filename = "twitter_queries/{0}.json".format(query)
 tweets_csvs_filename = "twitter_queries_csvs/{0}.json".format(query)
 
-with open(tweets_filename) as tweets_file:
-    for search_object in tweets_file:
-        search
-        print tweet_data
-        tweet = Tweet(tweet_data)
-        print tweet.text
-        break
-    
+with io.open(tweets_csvs_filename, 'ab') as csv_data:
+    writer = UnicodeWriter(csv_data)
+
+    with open(tweets_filename) as tweets_file:
+        for tweet_data in tweets_file:
+            tweet = Tweet(json.loads(tweet_data))
+            writer.writerow([
+                tweet.text,
+                str(tweet.id),
+                str(tweet.favorite_count),
+                str(','.join(tweet.user_screen_name_mentions)),
+                ','.join(tweet.user_name_mentions),
+                ','.join(tweet.hashtags),
+                ','.join(tweet.urls),
+                tweet.source,
+                tweet.user.followers_count(),
+                tweet.user.statuses_count(),
+                tweet.user.description(),
+                tweet.user.friends_count(),
+                tweet.user.location(),
+                tweet.user.screen_name(),
+                tweet.user.lang(),
+                tweet.user.name(),
+                tweet.user.created_at(),
+                tweet.user.time_zone(),
+                str(tweet.geo),
+                tweet.lang,
+                tweet.created_at,
+                json.dumps(tweet.place)
+            ])
+        
